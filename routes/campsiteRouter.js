9cc8413865ee6c47fba6938f5e7eb1cf881278ae
Bailey Stritzinger <bailey.stritzinger@gmail.com>
2022-08-27 12:01:11 -0400

    Assignment 1

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const campsiteRouter = express.Router();
 
+
+
 campsiteRouter.route('/')
 .all((req, res, next) => {
     res.statusCode = 200;
@@ -19,6 +21,26 @@ campsiteRouter.route('/')
 })
 .delete((req, res) => {
     res.end('Deleting all campsites');
+});
+
+campsiteRouter.route('/:campsiteId')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req, res) => {
+    res.end(`Will send details of the campsite: ${req.params.campsiteId} to you`);
+})
+.post((req, res) => {
+    res.end(`Post operation not supported on /campsites/${req.params.campsiteId}`)
+})
+.put((req, res) => {
+    res.statusCode = 403;
+    res.end(`Will update the campsite: ${req.body.name} with description: ${req.params.campsiteId}`);
+})
+.delete((req, res) => {
+    res.end(`Deleting campsites: ${req.params.campsiteId}`);
 });
 
 module.exports = campsiteRouter;
